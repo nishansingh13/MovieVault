9e5a36eadc884f1fe1fb11b82f85db0ad2460901
MayurMohan0654 <mayurmohan2486@gmail.com>
2025-04-10 18:05:00 +0530

    Register logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,37 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const registerUser = async (req, res) => {
+
+
+    const loginUser = async (req, res) => {
+        try {
+
+            const { email, password } = req.body;
+            if (!email || !password) {
+                return res.status(404).json({ error: "All fields are required" });
+            }
+            if(email=="admin" && password=="admin") {
+                const admintoken = jwt.sign({id:0,email:email,role:1,}, process.env.JWT_SECRET)
+                res.status(200).json({ message: "Admin logged in successfully", token: admintoken, role: 1 });
+                return;
+            }
+            const user = await User.findOne({ email });
+            if (!user) {
+                return res.status(404).json({ error: "User not found" });
+            }
+            const correctPass = await bcrypt.compare(password, user.password);
+            if (!correctPass) {
+                return res.status(400).json({ error: "Invalid credentials" });
+            }
+            const token = jwt.sign({ id: user._id, email: email ,role:0}, process.env.JWT_SECRET);
+
+            res.status(200).json({ message: "User logged in successfully", token: token , role: 0 });
+        } catch (error) {
+            res.status(500).json({ error: "Failed to fetch data", error: error.message });
+        }
+    };
+
+    const registerUser = async (req, res) => {
     try {
         const { username, email, password } = req.body;
         if (!username || !email || !password) {
@@ -26,31 +56,5 @@ const registerUser = async (req, res) => {
     }
 };
 
-    const loginUser = async (req, res) => {
-        try {
-            const { email, password } = req.body;
-            if(email=="admin" && password=="admin") {
-                const admintoken = jwt.sign({id:0,email:email,role:1,}, process.env.JWT_SECRET)
-                res.status(200).json({ message: "Admin logged in successfully", token: admintoken, role: 1 });
-                return;
-            }
-            if (!email || !password) {
-                return res.status(404).json({ error: "All fields are required" });
-            }
-            const user = await User.findOne({ email });
-            if (!user) {
-                return res.status(404).json({ error: "User not found" });
-            }
-            const correctPass = await bcrypt.compare(password, user.password);
-            if (!correctPass) {
-                return res.status(400).json({ error: "Invalid credentials" });
-            }
-            const token = jwt.sign({ id: user._id, email: email ,role:0}, process.env.JWT_SECRET);
-
-            res.status(200).json({ message: "User logged in successfully", token: token , role: 0 });
-        } catch (error) {
-            res.status(500).json({ error: "Failed to fetch data", error: error.message });
-        }
-    };
 
 module.exports = { registerUser, loginUser };
